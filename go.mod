6cddfb4521a43c916c56b25ef00d4d4daa1011a8
Christopher Koleda <koleda.christopher@gmail.com>
2019-07-16 11:06:47 -0400

    Init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ko1eda/backupmanager
+
+go 1.12
+
+require (
+	github.com/aws/aws-sdk-go v1.20.20
+	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// Parse incoming requsts
+	// Get hashed query parameter and decrpyt it
+	// create new IAM user
+	// create new S3 bucket
+	// create new IAM Policy
+	// return credentials to user
+}
