ecf26b4d13c73508a1dafa3a89c560ded3e36f1b
Christopher Koleda <koleda.christopher@gmail.com>
2019-07-31 13:08:56 -0400

    Add x-forwarded for check in SecretKey middleware

--- http/middlewares.go
+++ http/middlewares.go
@@ -3,6 +3,7 @@ package http
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 // SecretKeyValidation validates that the incoming request has a valid secret_key paramter
@@ -10,8 +11,12 @@ func (s *Server) secretKeyValidation(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		secret := r.URL.Query().Get("secret_key")
 
+		// try and get any nafarious ip addresses from the request
+		ff := r.URL.Query().Get("X-Forwarded-For")
+		ips := strings.Split(ff, ",")
+
 		if s.Validator.Validate(secret) != true {
-			log.Println("InvalidSecretKeyRequest:")
+			log.Println("InvalidSecretKeyRequest: IPS - ", ips)
 
 			w.WriteHeader(http.StatusBadRequest)
 			return
